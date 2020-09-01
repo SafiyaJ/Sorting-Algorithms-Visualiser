e134e6224fb81b063252d67a88d306b1919e7670
SafiyaJ <Safiya_Jama@hotmail.co.uk>
2020-09-01 12:53:13 +0200

    Updated the quick sort algorithm

--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {getMergeSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js';
+import {getQuickSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js';
 import './SortingVisualiser.css'
 
 // Change this value for the speed of the animations.
@@ -54,7 +55,29 @@ export default class SortingVisualiser extends React.Component {
         }
     }
 
-    quickSort() {}
+    quickSort() {
+        const animations = getQuickSortAnimations(this.state.array);
+        for (let i= 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i% 3 === 0 ? 'navy' : 'pink';
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color; 
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
+        }
+    }
 
     heapSort() {}
 
